b9b69e5495079a36bcff12562a37fe19ecc3ef67
Liviu <liviu.bancila95@gmail.com>
2024-10-04 09:48:22 +0300

    add main files

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 class RateLimiter {
     constructor(options = {}) {
-        this.windowMs = options.windowMs || 60000; // default 1 minute
-        this.maxRequests = options.max || 100; // default 100 requests per windowMs
+        this.windowMs = options.windowMs || 60000;
+        this.maxRequests = options.max || 100;
         this.message = options.message || "Too many requests, please try again later.";
-        this.store = options.store || new MemoryStore(); // use in-memory store by default
+        this.store = options.store || new MemoryStore();  // Update to use ESM import
     }
 
-    // Middleware function
     limit() {
         return async (req, res, next) => {
             try {
-                const key = req.ip; // Default key is IP. You can change this to user ID or something else
+                const key = req.ip;
                 const requestCount = await this.store.increment(key, this.windowMs);
 
                 if (requestCount > this.maxRequests) {
@@ -27,34 +26,5 @@ class RateLimiter {
     }
 }
 
-// Memory store implementation
-class MemoryStore {
-    constructor() {
-        this.hits = {};
-    }
-
-    increment(key, windowMs) {
-        return new Promise((resolve) => {
-            const currentTime = Date.now();
-            const resetTime = currentTime + windowMs;
-
-            if (!this.hits[key]) {
-                this.hits[key] = { count: 1, resetTime: resetTime };
-                return resolve(1);
-            }
-
-            const data = this.hits[key];
-
-            if (currentTime > data.resetTime) {
-                // Reset counter if the window has passed
-                this.hits[key] = { count: 1, resetTime: resetTime };
-                return resolve(1);
-            } else {
-                this.hits[key].count++;
-                return resolve(this.hits[key].count);
-            }
-        });
-    }
-}
-
-module.exports = RateLimiter;
+// Use ESM export
+export default RateLimiter;
